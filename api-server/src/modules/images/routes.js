6b31e4d25de39d0c4c155eb8c9c4d260e8467feb
Chris Greg <christickhubs@gmail.com>
2026-05-26 11:06:13 +0000

    feat: add protect middleware to upload route

--- api-server/src/modules/images/routes.js
+++ api-server/src/modules/images/routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import upload from "./internal/middleware/upload.js";
 import ImageController from "./internal/controllers/imageController.js";
+import { protect } from "../users/publicApi.js";
 
 const router = Router();
 
-router.post("/upload", upload.single("image"), ImageController.uploadImage);
+router.post("/upload", protect, upload.single("image"), ImageController.uploadImage);
 
 export default router;
 
